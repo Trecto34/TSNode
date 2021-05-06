df37fc41790f025a555c0a778984b471f9dc31a7
Trecto34 <anonymu616@gmail.com>
2021-05-06 14:20:58 -0300

    add .gitignore
    Signed-off-by: Trecto34 <anonymu616@gmail.com>

--- dist/server.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-function _interopRequireDefault (obj) {
-  return obj && obj.__esModule ? obj : { default: obj }
-}
-const _app = require('./app')
-const _app2 = _interopRequireDefault(_app)
-
-_app2.default.listen(3333)
